acb8ee6c25b39fbb49e1986f4605b65d27210322
Matteo Borghi <matteosimoneborghi@gmail.com>
2020-11-23 17:54:30 +0100

    fixed for condition in Apartments Seeder

--- database/seeds/ApartmentsTableSeeder.php
+++ database/seeds/ApartmentsTableSeeder.php
@@ -14,7 +14,7 @@ class ApartmentsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i > 10; $i++) {
+        for ($i = 0; $i < 10; $i++) {
 
             $randomUser = User::inRandomOrder()->first();
 
