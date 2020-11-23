961d607bac4fd778e4eb16a9fbd67021aec7b6a6
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-11-23 10:49:57 +0100

    message table

---
+++ database/migrations/2020_11_23_094427_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     * * Messages Table
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('apartment_id')->constrained();
+            $table->string('email');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
