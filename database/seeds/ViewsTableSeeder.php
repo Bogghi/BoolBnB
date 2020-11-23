cb84005232cf0abb6632570ac4e61d721ec0aea2
lorenzo-damico <loredam@hotmail.it>
2020-11-23 17:05:04 +0100

    Created views' seeder

---
+++ database/seeds/ViewsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\View;
+use App\Apartment;
+use Faker\Generator as Faker;
+
+class ViewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * Run this seeder to add some views in the database's views table.
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $apartments = Apartment::all();
+
+        foreach ($apartments as $apartment) {
+
+          $numberViews = rand(0, 20);
+
+          for ($i=0; $i < $numberViews; $i++) {
+
+            $newView = new View();
+
+            $newView->apartment_id = $apartment->id;
+            $newView->date = $faker->dateTimeThisYear('now', null);
+
+            $newView->save();
+
+          }
+        }
+    }
+}
