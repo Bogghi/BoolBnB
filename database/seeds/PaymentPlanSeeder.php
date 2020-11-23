fa75e3a2944c78f5d9885f0e7da34a0ae2bf5b55
Danilo Patanè <d.patane98@gmail.com>
2020-11-23 16:29:08 +0100

    Payment plan seeder done

---
+++ database/seeds/PaymentPlanSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\PaymentPlan;
+
+class PaymentPlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+
+            $durations = [24, 72, 144];
+
+            foreach ($durations as $duration) {
+                $newPayment = new PaymentPlan;
+
+                $newPayment->hours_duration = $duration;
+                
+                if ($duration == 24) {
+
+                    $newPayment->price = 2.99;
+
+                } elseif ($duration == 72) {
+                    $newPayment->price = 5.99;
+                } else {
+                    $newPayment->price = 9.99;
+                    
+                }
+                $newPayment->save();
+            }
+
+        
+    }
+}
