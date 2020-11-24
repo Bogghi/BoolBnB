39bf5dc6b9181644bf1df7c6369892c5402bfb68
Danilo Patanè <d.patane98@gmail.com>
2020-11-24 11:30:07 +0100

    Index function crud done and routed

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group(function () {
+    Route::resource('apartment', 'ApartmentController');
+
+});
