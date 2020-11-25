3ece8df97cea6536351f73db34c7e6a85897f1d7
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-11-25 17:19:05 +0100

    Correction responsive hamburger and apartment's admin

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,12 +1,15 @@
 {{-- Header top --}}
 <header>
-    <nav class="navbar navbar-expand-md navbar-light bg-white">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white zindex">
         <div class="container">
+            {{-- logo --}}
             <div class="logo">
                 <a href="{{ route('homepage') }}">
                     <img id="logo" src="{{ asset('img/boolbnb-logo-dark.svg') }}" alt="BoolBnB">
                 </a>
             </div>
+
+            {{-- hamburger --}}
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
@@ -15,23 +18,23 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
-
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto ">
-                    <!-- Authentication Links -->
+                <ul class="navbar-nav">
+
+                    <!-- Authentication Links  only admin is log-->
                     @if (Route::currentRouteName() == 'admin.apartment.index')
-                        <li class="nav-item textblue-hover">
+                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.apartment.create') }}">Create a new appartment</a>
                         </li>
                     @endif
+                    <!-- Authentication Links  only admin is log-->
                     @if (Route::currentRouteName() != 'homepage')
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('homepage') }}">Home</a>
                         </li>
                     @endif
-
 
                     @guest
                         <li class="nav-item">
@@ -49,8 +52,10 @@
                                 {{ Auth::user()->name }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            {{-- dropdown menu --}}
+                            <div class="dropdown-menu dropdown-menu-right mb-5" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('admin.apartment.index') }}">I tuoi appartamenti</a>
+                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
@@ -63,6 +68,7 @@
                         </li>
                     @endguest
                 </ul>
+
             </div>
         </div>
     </nav>
