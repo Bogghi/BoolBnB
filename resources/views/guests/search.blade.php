daa7c70d7ef57589bdc202e1aa52a5db2ac6c0bc
Danilo Patanè <d.patane98@gmail.com>
2020-11-26 23:43:17 +0100

    Homepage wip

--- resources/views/guests/search.blade.php
+++ resources/views/guests/search.blade.php
@@ -4,15 +4,44 @@
     <section>
 
         {{-- container pagina --}}
-        <div class="container-fluid">
+        <div class="container-fluid search-container">
 
             <div class="row">
-                <div class="col-8">
+                <div class="col-12 col-md-7">
                     @for ($i = 0; $i < 5; $i++)
                         
                     
                     <div class="d-flex flex-column">
-                        <img class="border rounded search-image" src="{{$all_sponsorized_apartments[$i]->cover_image}}" alt="Cover">
+                            <?php
+                                $image = $all_sponsorized_apartments[$i]->cover_image;
+                                $pos = strpos($image, "placeholder");
+                    
+                            ?> 
+                        <div class="d-flex border rounded container-apartment">
+                            <?php if ($pos === false) {?>
+
+                            <img class="border rounded search-image" src="{{asset('storage/'.$image)}}" alt="cover">
+                            
+                            <?php } else {?>
+                                
+                            <img class="border rounded search-image" src="{{$all_sponsorized_apartments[$i]->cover_image}}" alt="Cover">
+                            <?php }?>
+                            <div class="info-apartment d-flex flex-column">
+
+                            
+
+                                <h4 class="text-center">{{$all_sponsorized_apartments[$i]->title}}</h4>
+                                <p class="pl-2 info-text"><small>{{$all_sponsorized_apartments[$i]->description}}</small></p>
+                                <div class="info-tag d-flex">
+                                    <ul class="list-info">
+                                        <li><p> Address: {{$all_sponsorized_apartments[$i]->address}} </p></li>
+                                        <li><p> Beds number:  {{$all_sponsorized_apartments[$i]->beds_number}} </p></li>
+                                        <li><p> Square meters: {{$all_sponsorized_apartments[$i]->square_meters}}</p></li>
+                                        
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
                     @endfor
                 </div>
