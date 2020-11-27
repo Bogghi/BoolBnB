78259c2c8550592ba75e0912574604afc5c3f065
Andrea Contestabile <andreacontest@gmail.com>
2020-11-27 13:11:17 +0100

    Homepage sponsored section

--- resources/views/guests/homepage.blade.php
+++ resources/views/guests/homepage.blade.php
@@ -7,7 +7,7 @@
           <div class="col-sm-6 mx-auto">
             <div class="info-jumbo text-center">
               <h1 class="display-4">BoolBnB</h1>
-              <p class="lead">The website of your dream</p>
+              <p class="lead">The website of your dreams</p>
             </div>
             <div class="searchbar mt-5">
               <form class="form-inline d-flex justify-content-center md-form form-sm form-color mt-2" method="GET">
@@ -21,27 +21,15 @@
         </div>
       </div>
     </section>
-    <section class="container-fluid" id="sponsorizations">
-      
-          <div class="image-sponsored ml-5 pt-5">
-              @foreach ($sponsored_apartments as $apartments)
-
-              <?php
-              $image = $apartments->cover_image;
-              $pos = strpos($image, "placeholder");
-    
-              ?> 
-              
-              <div class="border mr-5 d-flex rounded sponsored">
-                <?php if ($pos === false) {?>
-                  <img class="responsive-image" src="{{asset('storage/'.$image)}}" alt="image">
-      
-                <?php } else {?>
-                    <img class="responsive-image" src="{{$apartments->cover_image}}" alt="image">
-              </div>
-              <?php }?>
-
-              @endforeach
-            </div>
+    <section id="sponsorizations" class="pt-4">
+      <h2 class="text-center mb-5">Appartamenti in evidenza</h2>
+      <div class="container-fluid d-flex flex-no-wrap">
+        @foreach ($sponsored_apartments as $apartment)
+        <div class="sponsored-item flex-shrink-0" style="background-image: url('https://dfdnews.com/uploads/istockphoto-1026205392-612x612.jpg')">
+        </div>    
+        @endforeach
+        
+      </div>
+          
     </section>
 @endsection
