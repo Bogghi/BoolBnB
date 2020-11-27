ec4849e077a46b02442dcf54d950c88e0d098e52
lorenzo-damico <loredam@hotmail.it>
2020-11-27 11:52:23 +0100

    Created route for the search api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route for the API search controller.
+Route::get('/search', 'Api\SearchController@search');
