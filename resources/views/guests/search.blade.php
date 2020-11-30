d75cef07bf4aef75ad2a08df568b72bccaae98b5
lorenzo-damico <loredam@hotmail.it>
2020-11-30 14:12:57 +0100

    search view indentation

--- resources/views/guests/search.blade.php
+++ resources/views/guests/search.blade.php
@@ -1,137 +1,153 @@
 @extends('layouts.guests')
 
 @section('content')
-    <section>
+<section>
 
-        {{-- container pagina --}}
-        <div class="container-fluid search-container">
-            {{-- ROW --}}
-            <div class="row">
-                {{-- Lista appartamenti --}}
+  {{-- container pagina --}}
+  <div class="container-fluid search-container">
 
-                <div class="col-12 col-md-5">
-                    {{-- searchbar --}}
+    {{-- ROW --}}
+    <div class="row">
 
+      {{-- Lista appartamenti --}}
+      <div class="col-12 col-md-5">
 
-                    <div class="searchbar my-3">
-                            <div id="search-input"></div>
-                                 @foreach ($services as $service)
-                                <div class="form-check form-check-inline">
-                                <input class="form-check-input services" type="checkbox" id="{{$service->id}}" name="services[]" value="{{$service->id}}">
-                                <label class="form-check-label" for="{{$service->id}}">{{$service->name}}</label>
-                                </div>
-                                @endforeach
-                                <div class="form-check form-check-row"  >
-                                    <label for="rooms_number">Rooms number</label>
-                                    <input name="rooms_number" id="rooms_number" class="form-control mb-2 m-sm-2" type="number">
-                                    <label for="beds_number">beds number</label>
-                                    <input name="beds_number" id="beds_number" class="form-control mb-2 m-sm-2" type="number">
-                                    <label for="radius">search radius</label>
-                                    <input name="radius" value="20" id="radius" class="form-control mb-2 m-sm-2" type="number">
+        {{-- searchbar --}}
+        <div class="searchbar my-3">
 
+          <div id="search-input"></div>
 
-                                </div>
-                            <button id="search-button" class="btn btn-primary">Cerca</button>
-                        </div>
+            @foreach ($services as $service)
+            <div class="form-check form-check-inline">
+              <input class="form-check-input services" type="checkbox" id="{{$service->id}}" name="services[]" value="{{$service->id}}">
+              <label class="form-check-label" for="{{$service->id}}">{{$service->name}}</label>
+            </div>
+            @endforeach
 
-                    {{-- searchbar --}}
+            <div class="form-check form-check-row"  >
+              <label for="rooms_number">Rooms number</label>
+              <input name="rooms_number" id="rooms_number" class="form-control mb-2 m-sm-2" type="number">
+              <label for="beds_number">beds number</label>
+              <input name="beds_number" id="beds_number" class="form-control mb-2 m-sm-2" type="number">
+              <label for="radius">search radius</label>
+              <input name="radius" value="20" id="radius" class="form-control mb-2 m-sm-2" type="number">
+            </div>
 
-                    <div class="list-apartment">
-                        <h2 class="text-center py-5">Sponsorized</h2>
-                        @for ($i = 0; $i < 5 && $i < count($all_sponsorized_apartments); $i++)
+            <button id="search-button" class="btn btn-primary">Cerca</button>
 
+        </div>
+        {{-- searchbar --}}
 
-                        <div class="d-flex flex-column">
-                                <?php
-                                    $image = $all_sponsorized_apartments[$i]->cover_image;
-                                    $pos = strpos($image, "placeholder");
+        <div class="list-apartment">
 
-                                ?>
-                            <div class="d-flex border rounded  container-apartment">
-                                <?php if ($pos === false) {?>
-                                <div class="search-image">
-                                <img class="border rounded" width="180px" height="180px" src="{{asset('storage/'.$image)}}" alt="cover">
-                                </div>
-                                <?php } else {?>
-                                <div class="search-image">
-                                <img class="border rounded" width="180px" height="180px" src="{{$all_sponsorized_apartments[$i]->cover_image}}" alt="Cover">
-                                </div>
-                                <?php }?>
-                                <div class="info-apartment d-flex flex-column">
+          <h2 class="text-center py-5">Sponsorized</h2>
 
+          @for ($i = 0; $i < 5 && $i < count($all_sponsorized_apartments); $i++)
+          <div class="d-flex flex-column">
 
+            <?php
+              $image = $all_sponsorized_apartments[$i]->cover_image;
+              $pos = strpos($image, "placeholder");
+            ?>
 
-                                    <h5 class="text-center">{{$all_sponsorized_apartments[$i]->title}}</h5>
-                                    <p class="pl-2 info-text">{{$all_sponsorized_apartments[$i]->description}}</p>
-                                    <div class="info-tag d-flex">
-                                        <ul class="list-info d-flex">
-                                            <li><p> <small> Address: {{$all_sponsorized_apartments[$i]->address}} </small> </p></li>
-                                            <li><p> <span><i class="fas fa-circle"></i></span><small> Beds number:  {{$all_sponsorized_apartments[$i]->beds_number}} </small></p></li>
-                                            <li><p> <span><i class="fas fa-circle"></i></span><small> Square meters: {{$all_sponsorized_apartments[$i]->square_meters}}</small></p></li>
-                                        </ul>
-                                            @if ($all_sponsorized_apartments[$i]->sponsorizations[0]->end_date)
-                                                <div id="sponsorized">
-                                                    <h4><span class="badge badge-success">Superhost</span></h4>
-                                                    </div>
-                                                @else
+            <div class="d-flex border rounded  container-apartment">
 
-                                            @endif
+              @if ($pos === false) {
+              <div class="search-image">
+                <img class="border rounded" width="180px" height="180px" src="{{asset('storage/'.$image)}}" alt="cover">
+              </div>
+              @else
+              <div class="search-image">
+                <img class="border rounded" width="180px" height="180px" src="{{$all_sponsorized_apartments[$i]->cover_image}}" alt="Cover">
+              </div>
+              @endif
 
-                                    </div>
-                                </div>
-                            </div>
+              <div class="info-apartment d-flex flex-column">
 
-                        </div>
-                        @endfor
-                        <h2 class="text-center py-5">Risultati</h2>
-                        @foreach ($apartments as $apartment)
+                <h5 class="text-center">{{$all_sponsorized_apartments[$i]->title}}</h5>
+                <p class="pl-2 info-text">{{$all_sponsorized_apartments[$i]->description}}</p>
 
-                        <div class="d-flex flex-column">
-                                <?php
-                                    $image = $apartment->cover_image;
-                                    $pos = strpos($image, "placeholder");
+                <div class="info-tag d-flex">
 
-                                ?>
-                            <div class="d-flex border rounded container-apartment">
-                                <?php if ($pos === false) {?>
-                                    <div class="search-image">
-                                    <img class="border rounded" width="180px" height="180px" src="{{asset('storage/'.$image)}}" alt="cover">
-                                    </div>
-                                    <?php } else {?>
-                                    <div class="search-image">
-                                    <img class="border rounded" width="180px" height="180px" src="{{$all_sponsorized_apartments[$i]->cover_image}}" alt="Cover">
-                                    </div>
-                                    <?php }?>
-                                <div class="info-apartment d-flex flex-column">
+                  <ul class="list-info d-flex">
+                    <li><p> <small> Address: {{$all_sponsorized_apartments[$i]->address}} </small> </p></li>
+                    <li><p> <span><i class="fas fa-circle"></i></span><small> Beds number:  {{$all_sponsorized_apartments[$i]->beds_number}} </small></p></li>
+                    <li><p> <span><i class="fas fa-circle"></i></span><small> Square meters: {{$all_sponsorized_apartments[$i]->square_meters}}</small></p></li>
+                  </ul>
 
+                  @if ($all_sponsorized_apartments[$i]->sponsorizations[0]->end_date)
+                  <div id="sponsorized">
+                    <h4><span class="badge badge-success">Superhost</span></h4>
+                  </div>
+                  @endif
 
-
-                                    <h5 class="text-center">{{$apartment->title}}</h5>
-                                    <p class="pl-2 info-text">{{$apartment->description}}</p>
-                                    <div class="info-tag d-flex">
-                                        <ul class="list-info d-flex">
-                                            <li><p><small> Address: {{$apartment->address}}</small> </p></li>
-                                            <li><p><span><i class="fas fa-circle"></i></span><small> Beds number:</small>  {{$apartment->beds_number}} </p></li>
-                                            <li><p><span><i class="fas fa-circle"></i></span><small> Square meters:</small> {{$apartment->square_meters}}</p></li>
-                                        </ul>
-                                            @if (count($apartment->sponsorizations->where('end_date', '>', date('Y-m-d h:m:s'))) == 1)
-                                            <div id="sponsorized">
-                                                <h4><span class="badge badge-success ">Superhost</span></h4>
-                                                </div>
-                                            @else
-
-                                        @endif
-
-
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        @endforeach
-                    </div>
                 </div>
-                {{-- Lista appartamenti --}}
+
+              </div>
+
+
+
+            </div>
+
+          </div>
+          @endfor
+
+          <h2 class="text-center py-5">Risultati</h2>
+
+          @foreach ($apartments as $apartment)
+          <div class="d-flex flex-column">
+
+            <?php
+              $image = $apartment->cover_image;
+              $pos = strpos($image, "placeholder");
+            ?>
+
+            <div class="d-flex border rounded container-apartment">
+
+              @if ($pos === false) {
+              <div class="search-image">
+                <img class="border rounded" width="180px" height="180px" src="{{asset('storage/'.$image)}}" alt="cover">
+              </div>
+              @else
+              <div class="search-image">
+                <img class="border rounded" width="180px" height="180px" src="{{$all_sponsorized_apartments[$i]->cover_image}}" alt="Cover">
+              </div>
+              @endif
+
+              <div class="info-apartment d-flex flex-column">
+
+                <h5 class="text-center">{{$apartment->title}}</h5>
+                <p class="pl-2 info-text">{{$apartment->description}}</p>
+
+                <div class="info-tag d-flex">
+
+                  <ul class="list-info d-flex">
+                    <li><p><small> Address: {{$apartment->address}}</small> </p></li>
+                    <li><p><span><i class="fas fa-circle"></i></span><small> Beds number:</small>  {{$apartment->beds_number}} </p></li>
+                    <li><p><span><i class="fas fa-circle"></i></span><small> Square meters:</small> {{$apartment->square_meters}}</p></li>
+                  </ul>
+
+                  @if (count($apartment->sponsorizations->where('end_date', '>', date('Y-m-d h:m:s'))) != 0)
+                  <div id="sponsorized">
+                    <h4><span class="badge badge-success ">Superhost</span></h4>
+                  </div>
+                  @endif
+
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+          @endforeach
+
+        </div>
+
+      </div>
+      {{-- Lista appartamenti --}}
+
                 {{-- Carousel --}}
                 <div class="d-none d-sm-block col-md-7 carousel-fixed">
                     <div id="carouselExampleIndicators" class="carousel slide carousel-pers" data-ride="carousel">
@@ -163,10 +179,11 @@
                 </div>
                 {{-- Carousel --}}
 
-            </div>
-            {{-- //ROW --}}
+    </div>
+    {{-- //ROW --}}
 
-        </div>
-        {{-- fine container pagina --}}
-    </section>
+  </div>
+  {{-- fine container pagina --}}
+
+</section>
 @endsection
