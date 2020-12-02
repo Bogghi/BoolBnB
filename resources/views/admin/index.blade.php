4b6b826ce7c5ee4a1076960ab0f0b758f9252ecc
Danilo Patanè <d.patane98@gmail.com>
2020-12-02 11:41:58 +0100

    Index-admin done

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container margin-top ">
-        <h1 class="pl-3 pt-3">Your Apartments:</h1>
+        <h1 class="pl-3 title-pers pt-3">Your Apartments:</h1>
 
         {{-- Call to apartment's table and stamp apartment list of admin --}}
         @foreach ($apartments as $apartment)
@@ -19,17 +19,15 @@
                     ?> 
 
                     {{-- Cover Apartment --}}
-                    <div class="col-md-2 p-3 d-flex">
-                        <?php if ($pos === false) {?>
-                            <img class="align-items-center card-img rounded-personalized my-2" src="{{asset('storage/'.$image)}}" alt="image">
-                
-                            <?php } else {?>
-                                <img class="align-items-center card-img rounded-personalized my-2" src="{{$apartment->cover_image}}" alt="image">
-                        <?php }?>
-                    </div>
+                    <?php if ($pos === false) {?>
+                    <div class="col-12 col-md-2 p-3 d-flex rounded-personalized mt-4" style="background-image: url({{asset('storage/'.$apartment->cover_image)}})"></div>
+                    <?php } else {?>
+                    <div class="col-12 col-md-2 p-3 d-flex rounded-personalized mt-4" style="background-image: url({{$apartment->cover_image}})"></div>    
+                    <?php }?>
+                    
                    
                     {{-- Description Apartments --}}
-                    <div class="col-md-9">
+                    <div class="col-12 col-md-10">
 
                         {{-- Description --}}
                         <div class="card-body">
@@ -40,7 +38,7 @@
                             <p class="card-text"><small class="text-muted">Created on {{$apartment->created_at->format('d-m-Y')}}.</small></p>
                         </div>
                         {{-- {{-- bottom for link pages --}}
-                        <div  class="d-flex flex-wrap justify-content-end align-items-end   mb-1">
+                        <div  class="d-flex flex-wrap link-personalized mb-1">
                             <a href="{{route('admin.apartment.show', $apartment)}}" class="btn-personalized btn mr-2 mb-2">Show</a>
                             <a href="{{route('admin.apartment.edit', $apartment)}}" class="btn-personalized btn mr-2 mb-2">Edit</a>
                             <a href="#" class="btn-personalized btn mr-2 mb-2">Statistic</a>
