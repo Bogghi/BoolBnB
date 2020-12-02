0845a7824ac90dbcfe84768db8ff10d0b94ed200
lorenzo-damico <loredam@hotmail.it>
2020-12-02 10:46:03 +0100

    add date column to message table

--- database/seeds/MessagesSeeder.php
+++ database/seeds/MessagesSeeder.php
@@ -9,20 +9,21 @@ class MessagesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     * 
+     *
      * * Message from User
      *
      * @return void
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 100; $i++) {
 
             $newMessage = new Message;
 
             $newMessage->apartment_id = Apartment::inRandomOrder()->first()->id;
             $newMessage->email = $faker->email();
             $newMessage->content = $faker->text(200);
+            $newMessage->date = $faker->dateTimeThisYear('now', null);
 
             $newMessage->save();
         }
