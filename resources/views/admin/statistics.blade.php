b1d289a99173e01ad508b73e816ac8450aa2a0ec
Matteo Borghi <matteosimoneborghi@gmail.com>
2020-12-02 17:37:00 +0100

    completed the integration and styling

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -2,8 +2,31 @@
 
 @section('content')
 
-<canvas id="chartjs-0" class="chartjs" width="770" height="385" style="display: block; width: 770px; height: 385px;"></canvas>
+<section class="charts">
 
-<canvas id="myChart" width="400" height="400"></canvas>
+    <div class="content">
+
+        <h1>Your apartment statistics</h1>
+        <p>Over here you find the average views and messages that you apartment had has during the past 10 years divided by months</p>
+
+        <div class="stats-title">
+
+
+        </div>
+
+        <div class="stats">
+            <div class="stats-box">
+                <h2>Views</h2>
+                <canvas id="chartjs-0"></canvas>
+            </div>
+            <div class="stats-box">
+                <h2>Messages</h2>
+                <canvas id="myChart"></canvas>
+            </div>
+        </div>
+        
+    </div>
+
+</section>
 
 @endsection
