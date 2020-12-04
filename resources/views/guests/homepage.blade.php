803dbcdc65c8ef1a514f89ac1ae340e99291cd18
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-12-04 16:00:12 +0100

    Cities Main

--- resources/views/guests/homepage.blade.php
+++ resources/views/guests/homepage.blade.php
@@ -114,11 +114,61 @@
 
     </section>
 
-    {{-- Apartment mix --}}
-    <section class="pt-5 bg-ligth">
-      <h2 class="text-center mb-5">Our Apartments</h2>
-      
-  </section>
+
+     {{-- Cities mix --}}
+     <section class="pt-5 bg-ligth container-fluid">
+        <h2 class="text-center mb-5">Best Cities</h2>
+        <div class="row">
+            {{-- box cities --}}
+            <div class="col-lg-2 col-md-4 col-6 text-center">
+                <h2>Parigi</h2>                
+                <a  href="#">
+                    <div class="box-cities" style="background-image: url(http://www.competitiontravel.it/wp-content/uploads/2014/09/Parigi-2-1024x683.jpg)">
+                    </div>
+                </a> 
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6 text-center">
+                <h2>Roma</h2>                
+                <a  href="#">
+                    <div class="box-cities" style="background-image: url(https://www.fodors.com/wp-content/uploads/2018/10/HERO_UltimateRome_Hero_shutterstock789412159.jpg)">
+                    </div>
+                </a> 
+            </div>
+            
+            <div class="col-lg-2 col-md-4 col-6 text-center">
+                <h2>Londra</h2>                
+                <a  href="#">
+                    <div class="box-cities" style="background-image: url(https://lh3.googleusercontent.com/proxy/b-7H2WubUDt70ycwlYPDPDo0EpdmHfIDPPLsvnpQgChscpS_1P4AdmAgNtBx7gfAz0VzrHIxomwUWObz3LNcjAy8enyHEZSsmu2dxbhN9TRR0IQIIxGS2Fsl3xkRxP0J6MISWZAcYS29RHOVqFGeotZSggsFJXQn7GYsw_XY8OA6yQp8qDWazoE6jw)">
+                    </div>
+                </a> 
+            </div>
+            <div class="col-lg-2 col-md-4 col-6 text-center">
+                <h2>Barcellona</h2>                
+                <a  href="#">
+                    <div class="box-cities" style="background-image: url(https://www.voglioviverecosi.com/wp-content/uploads/2019/09/LAVORO-BARCELLONA-1900x1080.jpg)">
+                    </div>
+                </a> 
+            </div>
+            <div class="col-lg-2 col-md-4 col-6 text-center">
+                <h2>Berlino</h2>                
+                <a  href="#">
+                    <div class="box-cities" style="background-image: url(https://www.robintur.it/img/viaggi/germania/image-thumb__6491__galleryCarousel/germania-berlino-porta-di-brandeburgo~-~300w.pjpeg)">
+                    </div>
+                </a> 
+            </div>
+            <div class="col-lg-2 col-md-4 col-6 text-center">
+                <h2>Vienna</h2>                
+                <a  href="#">
+                    <div class="box-cities" style="background-image: url(https://www.10cose.it/wp-content/uploads/2015/11/vienna-696x456.jpg)">
+                    </div>
+                </a> 
+            </div>
+
+
+        </div>
+
+    </section>
 
     {{-- Developer pics team --}}
     <section id="developers">
