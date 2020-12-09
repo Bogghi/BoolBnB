a27ced3390e1652ea86a9a5a2be124e011ed79e1
Danilo Patanè <d.patane98@gmail.com>
2020-12-09 12:34:38 +0100

    Double column

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -80,35 +80,44 @@
     </div>
     {{-- row carousel  --}}
     {{-- row info --}}
-    <div class="row mb-5">
-      <div class="col-12 col-md-7 border-pers">
-        <h2 class="py-2">Description</h2>
-        <p>{{$apartment->description}}</p>
-      </div>
-      <div class="col-12 col-md-5 services">
-        <h2>Services</h2>
-        <ul class="d-flex flex-column flex-wrap">
-          @foreach ($apartment->services as $service)
-          <li><span><i class="fas fa-circle dot-pers mr-2"></i></span>{{$service->name}}</li>
-          @endforeach
-        </ul>
+    <div class="container-info d-flex">
+      <div class="container-show" >
+        <div class="row mb-5">
+          <div class="col-12 col-md-12 border-pers">
+            <h2 class="py-2">Description</h2>
+            <p>{{$apartment->description}}</p>
+          </div>
+          <div class="col-12 col-md-12 services">
+            <h2>Services</h2>
+            <ul class="d-flex flex-column flex-wrap">
+              @foreach ($apartment->services as $service)
+              <li><span><i class="fas fa-circle dot-pers mr-2"></i></span>{{$service->name}}</li>
+              @endforeach
+            </ul>
+          </div>
+          
+          <div class="col-12 col-md-12">
+            <div id="map-container"></div>
+
+          </div>
+
+        </div>
+        
       </div>
       
-      <div class="col-12 col-md-6">
-        <div id="map-container"></div>
-
-      </div>
-
-
       @if (Auth::id() == $apartment->user_id )
-      <div class="col-12 offset-md-1 col-md-5 message">
-        <h2 class="pb-3 pt-3">Messaggi ricevuti</h2>
-        <ul>
-          @foreach ($apartment->messages as $message)
-          <li data-toggle="modal" data-target="#staticBackdrop" data-id="{{$message->id}}" class="single-message pb-2 d-flex"><span class="align-middle"><i class="fas fa-circle dot-pers mr-2"></i></span><p>{{$message->email}}</p></li>
-          @endforeach
-        </ul>
-      </div>
+
+
+        
+        <div class="message">
+          <h2 class="pb-3 pt-3">Messaggi ricevuti</h2>
+          <ul>
+            @foreach ($apartment->messages as $message)
+            <li data-toggle="modal" data-target="#staticBackdrop" data-id="{{$message->id}}" class="single-message pb-2 d-flex"><span class="align-middle"><i class="fas fa-circle dot-pers mr-2"></i></span><p>{{$message->email}}</p></li>
+            @endforeach
+          </ul>
+        </div>
+      
 
       {{-- MODAL --}}
       <div class="modal fade color-primary modal-tr" id="staticBackdrop" data-backdrop="static" data-keyboard="false"tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -134,7 +143,7 @@
 
       @else
 
-      <div class="col-12 offset-md-1 col-md-5 contatti">
+      <div class="col-12 col-md-12 contatti">
 
         <form class="padding-pers" action="{{route("message.store", $apartment->id)}}" method="POST">
           @csrf
@@ -157,14 +166,6 @@
 
       </div>
       @endif
-
-
-
-
-
-
-
-
     </div>
   </div>
 </section>
